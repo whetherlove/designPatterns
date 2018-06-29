1734efd31079921478e29eb3faee36a38ab87ce9
Jacob Zhang <274381933@qq.com>
2018-06-29 21:06:10 +0800

    add facade pattern

---
+++ src/GoF/facadePattern/homeTheater/HomeTheaterTest.java
@@ -0,0 +1,33 @@
+package GoF.facadePattern.homeTheater;
+
+import GoF.facadePattern.homeTheater.devices.*;
+
+/**
+ * @ProjectName: designPatterns
+ * @Package: GoF.facadePattern.homeTheater
+ * @Description:
+ * @Author: Jacob Zhang
+ * @CreateDate: 2018/6/29/029 20:55
+ * @UpdateDate: 2018/6/29/029 20:55
+ */
+public class HomeTheaterTest {
+
+    public static void main(String[] args) {
+
+        Amplifier amp = new Amplifier("Top-O-Line Amplifier");
+        Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner", amp);
+        DvdPlayer dvd = new DvdPlayer("Top-O-Line DVD Player", amp);
+        CdPlayer cd = new CdPlayer("Top-O-Line CD Player", amp);
+        Projector projector = new Projector("Top-O-Line Projector", dvd);
+        TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
+        Screen screen = new Screen("Theater Screen");
+        PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+
+        HomeTheaterFacade homeTheater =
+                new HomeTheaterFacade(amp, tuner, dvd, cd,
+                        projector, lights, screen, popper);
+
+        homeTheater.watchMovie("Raiders of the Lost Ark");
+        homeTheater.endMovie();
+    }
+}
